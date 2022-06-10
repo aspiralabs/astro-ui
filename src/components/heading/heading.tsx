68c89fe51e1dc4729111eb2f5929315e44114e28
dludemann <dludemann@aspiralabs.co>
2022-06-10 14:35:59 -0400

    add tailwind overrider to heading

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -1,27 +1,30 @@
 // Generated with util/create-component.js
 import React from 'react';
+import { overrideTailwindClasses } from 'tailwind-override';
 
 import { TitleProps } from './heading.types';
 
 const Title: React.FC<TitleProps> = ({ children, as = 'h1', className = '' }) => {
+    const finalClassName = overrideTailwindClasses(`text-heading font-heading ${className}`);
+
     switch (as) {
         case 'h1': {
-            return <h1 className={`text-heading  font-heading   ${className}`}>{children}</h1>;
+            return <h1 className={finalClassName}>{children}</h1>;
         }
         case 'h2': {
-            return <h2 className={`text-heading font-heading   ${className}`}>{children}</h2>;
+            return <h2 className={finalClassName}>{children}</h2>;
         }
         case 'h3': {
-            return <h3 className={`text-heading font-heading  ${className}`}>{children}</h3>;
+            return <h3 className={finalClassName}>{children}</h3>;
         }
         case 'h4': {
-            return <h4 className={`text-heading font-heading  ${className}`}>{children}</h4>;
+            return <h4 className={finalClassName}>{children}</h4>;
         }
         case 'h5': {
-            return <h5 className={`text-heading font-heading   ${className}`}>{children}</h5>;
+            return <h5 className={finalClassName}>{children}</h5>;
         }
         case 'h6': {
-            return <h6 className={`text-heading font-heading  ${className}`}>{children}</h6>;
+            return <h6 className={finalClassName}>{children}</h6>;
         }
     }
 };
